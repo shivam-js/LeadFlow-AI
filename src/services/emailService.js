c902c00b71754279182887045e7ab723466d3a9a
shivam-js <prajapati2shivam@gmail.com>
2026-06-25 17:06:40 +0530

    Fixing EmailJS

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,5 +1,10 @@
 import emailjs from "@emailjs/browser";
 
+console.log("SERVICE:", import.meta.env.VITE_EMAILJS_SERVICE_ID);
+console.log("TEMPLATE:", import.meta.env.VITE_EMAILJS_TEMPLATE_ID);
+console.log("PUBLIC:", import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
+console.log("MODE:", import.meta.env.MODE);
+
 export const sendLeadEmail = async (lead) => {
   try {
     const response = await emailjs.send(
